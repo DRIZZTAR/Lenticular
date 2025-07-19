58c538dbdb607f3189db35a90ff1d86d7bc5869b
DRIZZTAR <tyson_jeremy@hotmail.com>
2025-07-19 12:16:52 -0600

    texture switching and blending added, including Leva for controls

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,51 @@
-import { OrbitControls, Environment } from '@react-three/drei';
+import {
+	OrbitControls,
+	Environment,
+	useVideoTexture,
+	useTexture,
+} from '@react-three/drei';
 import { LenticularMaterial } from './LenticularMaterial';
+import { useControls } from 'leva';
 
 export const Experience = () => {
+	const { textureSet } = useControls({
+		textureSet: {
+			value: 'charmander-squirtle',
+			options: ['charmander-squirtle', 'white-stripes'],
+		},
+	});
+
+	const videoA = useVideoTexture('textures/white-stripes/s.mp4');
+	const videoB = useVideoTexture('textures/white-stripes/ws.mp4');
+
+	const textureCharmanderSquirtleA = useTexture(
+		'textures/charmander-squirtle/charmander.png'
+	);
+	const textureCharmanderSquirtleB = useTexture(
+		'textures/charmander-squirtle/squirtle.png'
+	);
+
+	const textureA = {
+		['charmander-squirtle']: textureCharmanderSquirtleA,
+		['white-stripes']: videoA,
+	}[textureSet];
+
+	const textureB = {
+		['charmander-squirtle']: textureCharmanderSquirtleB,
+		['white-stripes']: videoB,
+	}[textureSet];
+
 	return (
 		<>
 			<OrbitControls />
 			<Environment preset='sunset' />
 			<mesh>
 				<planeGeometry args={[1, 1]} />
-				<LenticularMaterial />
+				<LenticularMaterial
+					key={textureSet}
+					textureA={textureA}
+					textureB={textureB}
+				/>
 			</mesh>
 		</>
 	);
--- src/components/LenticularMaterial.jsx
+++ src/components/LenticularMaterial.jsx
@@ -1,10 +1,27 @@
 import { extend } from '@react-three/fiber';
+import { useMemo } from 'react';
+import { color, mix, uv, texture } from 'three/tsl';
 import { MeshStandardNodeMaterial } from 'three/webgpu';
 
 extend({
 	MeshStandardNodeMaterial,
 });
 
-export const LenticularMaterial = () => {
-	return <meshStandardNodeMaterial color='pink' />;
+export const LenticularMaterial = ({
+  textureA,
+  textureB,
+}) => {
+	const { nodes, uniforms } = useMemo(() => {
+		const uniforms = {};
+    const texA = texture(textureA);
+    const texB = texture(textureB);
+		return {
+			uniforms,
+			nodes: {
+				colorNode: mix(texA, texB, uv().x),
+			},
+		};
+	}, []);
+
+	return <meshStandardNodeMaterial {...nodes} />;
 };
